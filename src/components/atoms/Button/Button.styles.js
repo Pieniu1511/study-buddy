892989fad892b5ac5913ce5b87545e3f550a5f63
Edmund <pieniu1511@gmail.com>
2023-02-06 21:41:11 +0100

    styled list

---
+++ src/components/atoms/Button/Button.styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import { theme } from "assets/styles/theme";
+
+export const StyledButton = styled.button`
+  width: 25px;
+  height: 25px;
+  background-color: ${theme.colors.grey};
+  border-radius: 50px;
+  border: none;
+  color: white;
+  margin: 0 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+`;
--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'components/atoms/Button/Button';
+import { StyledAverage, StyledInfo, Wrapper } from './UserListItem.styles';
 
 const UsersListItem = ({ userData: { average, name, attendance = '0%' } }) => {
   return (
-    <li>
-      <div>{average}</div>
-      <div>
-        <p>{name}</p>
-        <p>{attendance}</p>
-      </div>
-      <button>X</button>
-    </li>
+    <Wrapper>
+      <StyledAverage value={average}>{average}</StyledAverage>
+      <StyledInfo>
+        <p>
+          {name}
+          <Button />
+        </p>
+        <p>attendance: {attendance}</p>
+      </StyledInfo>
+    </Wrapper>
   );
 };
 
